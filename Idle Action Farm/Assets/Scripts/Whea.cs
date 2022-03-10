53222e8eb27608f0ff9be89b87c333a58a21cf83
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-03-10 17:43:15 +0300

    added Growth

--- Idle Action Farm/Assets/Scripts/Whea.cs
+++ Idle Action Farm/Assets/Scripts/Whea.cs
@@ -7,12 +7,43 @@ public class Whea : MonoBehaviour
 {
     [SerializeField] private GameObject _crap;
     
+    private bool isGrowth = false;
+    private bool IsGrowth{
+        get => isGrowth;
+        set{
+            GetComponent<BoxCollider>().enabled = !value;
+            isGrowth = value;
+        }
+    }
+    private float speed = 0.03f;
+    public Transform target;
     public void Cut(){
-        if (FindObjectOfType<GameManager>().AddCrop()){
+        if (FindObjectOfType<GameManager>().AddCrop() && !IsGrowth){
             FindObjectOfType<Points>().Add();
             Instantiate(_crap).GetComponent<Transform>().position = 
                 new Vector3(transform.position.x, _crap.GetComponent<Transform>().position.y, transform.position.z);
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            GameObject gm = new GameObject();
+            gm.transform.position = this.transform.position;
+            gm.transform.rotation = this.transform.rotation;
+
+            target = Instantiate(gm).GetComponent<Transform>();
+            IsGrowth = true;
+
+            transform.position = 
+                new Vector3(transform.position.x, transform.position.y - transform.position.y * 2, transform.position.z);
+        }
+    }
+
+    private void Update() {
+        if (IsGrowth){
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+            if (transform.position == target.position) {
+                Destroy(target.gameObject);
+                IsGrowth = false;
+            }
         }
     }
 }
